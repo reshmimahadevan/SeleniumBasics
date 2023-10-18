1eeab2b939727ab291224c9f90c2c0ef874d53d5
reshmimahadevan <148185732+reshmimahadevan@users.noreply.github.com>
2023-10-18 16:14:25 -0700

    Updated Firefox.java

--- Selenium_Basics/src/Firefox.java
+++ Selenium_Basics/src/Firefox.java
@@ -5,8 +5,6 @@ public class Firefox {
 
 	public static void main(String[] args) {
 
-		System.setProperty("webdriver.gecko.driver", "C:\\geckodriver_win32\\geckodriver.exe");
-
 		WebDriver driver = new FirefoxDriver();
 
 		driver.get("https://rahulshettyacademy.com");
